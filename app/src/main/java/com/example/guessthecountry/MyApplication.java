2ac0d5afe76a9d3c27764dfe4af6c8c70256219b
Ahmed Gavaz <93177410+ahmedgavaz@users.noreply.github.com>
2024-03-01 17:03:23 +0200

    Update MyApplication.java
    Adding melody to app

--- app/src/main/java/com/example/guessthecountry/MyApplication.java
+++ app/src/main/java/com/example/guessthecountry/MyApplication.java
@@ -1,14 +1,19 @@
 package com.example.guessthecountry;
 
 import android.app.Application;
+import android.media.MediaPlayer;
 
 public class MyApplication extends Application {
     private static MyApplication instance;
+    private MediaPlayer mediaPlayer;
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
+        mediaPlayer = MediaPlayer.create(this, R.raw.melody);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
     }
 
     public static MyApplication getInstance() {
