c679e8b3d3513fe34e7c1d697d6929cf0683113f
Ahmed Gavaz <93177410+ahmedgavaz@users.noreply.github.com>
2024-03-20 16:52:57 +0200

    Update User.java
    Добавяне на логин

--- app/src/main/java/com/example/guessthecountry/User.java
+++ app/src/main/java/com/example/guessthecountry/User.java
@@ -1,13 +1,45 @@
 package com.example.guessthecountry;
 
 public class User {
+    private int id;
     private String username;
+    private String password;
     private int points;
 
     public User(String username, int points) {
         this.username = username;
         this.points = points;
     }
+    public User(){
+    }
+    public User(int id, String Username, String _pass){
+        this.id = id;
+        this.username = Username;
+        this.password = _pass;
+    }
+    public User(String Username, String _pass){
+        this.username = Username;
+        this.password = _pass;
+    }
+    public int getID(){
+        return this.id;
+    }
+    public void setID(int id){
+        this.id = id;
+    }
+    public String getUserName(){
+        return this.username;
+    }
+    public void setUserName(String name){
+        this.username = name;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public void setPassword(String _pass){
+        this.password = _pass;
+    }
+
 
     public String getUsername() {
         return username;
